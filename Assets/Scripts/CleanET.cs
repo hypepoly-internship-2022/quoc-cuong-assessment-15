5b1cabf65fb610e8ddca88ff8b7440e92f140dc9
chimpuchim <96745654+chimpuchim@users.noreply.github.com>
2022-11-30 10:08:33 +0700

    +clean dirty

---
+++ Assets/Scripts/CleanET.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleanET : MonoBehaviour
+{
+
+    private Camera mainCamera;
+    private Rigidbody body;
+
+    private void Awake() 
+    {
+        mainCamera = Camera.main;    
+    }
+
+    private void FixedUpdate() 
+    {
+        blowAwayDirt(); 
+    }
+
+    private void blowAwayDirt()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            if(hit.collider.gameObject.CompareTag("Dirty"))
+            {
+                body = hit.collider.GetComponent<Rigidbody>();
+                randomWaterForce(body);
+                body.useGravity = true;
+                hit.collider.tag = "Clean";
+            }
+        }
+    }
+
+    private void randomWaterForce(Rigidbody body)
+    {
+        int ran = Random.Range(30, 40);
+        body.AddForce(0, ran * 3.5f, -ran * 1.5f);
+    }
+}
